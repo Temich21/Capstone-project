00ea421bb04aadea74519f18847ba2de7feb1a2e
Artem2108 <192549@vutbr.cz>
2023-07-12 14:18:34 +0200

    Some commit message of your choice goes here

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Phasellus et quam fringilla, pharetra nunc ut, ultrices tortor. Fusce porta neque eget nisl tempus ultrices. Donec vitae porttitor nulla. Proin tempor lectus leo, sed faucibus eros dictum in. Curabitur tempus metus sed arcu congue elementum. Curabitur vehicula nisi nec turpis venenatis tristique. Duis nisi urna, ornare eu pellentesque sit amet, fermentum a mauris. Nulla ante libero, bibendum quis metus sed, faucibus vehicula enim. Aliquam venenatis vehicula pellentesque. Nunc in lacus justo. Nunc lectus felis, hendrerit sit amet molestie lobortis, molestie a massa. Suspendisse velit elit, molestie in nunc quis, condimentum imperdiet felis. Ut luctus congue purus, vitae fermentum nibh placerat sit amet. Quisque facilisis elit ipsum, vitae auctor metus egestas id.
     </div>
   );
 }
